b77ab896522096ee51141f37d83c0423b179adbe
Jeff Balogh <jbalogh@mozilla.com>
2011-04-21 22:00:19 -0700

    make it slower

--- twttr/views.py
+++ twttr/views.py
@@ -4,17 +4,16 @@ from .models import User, Tweet
 
 
 def home(request):
-    tweets = Tweet.objects.order_by('-created')[:20]
+    tweets = Tweet.objects.order_by('-created')[:30]
     return render(request, 'twttr/base.html', {'tweets': tweets})
 
 
 def user(request, username):
-    user = get_object_or_404(User, screen_name=username)
-    tweets = Tweet.objects.filter(user=user).order_by('-created')[:20]
+    tweets = (Tweet.objects.filter(user__screen_name=username)
+              .order_by('-created'))[:30]
     return render(request, 'twttr/base.html', {'tweets': tweets})
 
 
 def tweet(request, username, id):
-    user = get_object_or_404(User, screen_name=username)
-    tweet = get_object_or_404(Tweet, user=user, id=id)
+    tweet = get_object_or_404(Tweet, user__screen_name=username, id=id)
     return render(request, 'twttr/base.html', {'tweets': [tweet]})
