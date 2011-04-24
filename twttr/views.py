2830c4c0028ddb1b4c269d029cd66998bbe90779
Jeff Balogh <jbalogh@mozilla.com>
2011-04-23 18:34:13 -0700

    more select related

--- twttr/views.py
+++ twttr/views.py
@@ -12,7 +12,7 @@ def home(request):
 
 def user(request, username):
     user_ = User.objects.get(screen_name=username)
-    tweets = (Tweet.objects.filter(user=user_).order_by('-created'))[:30]
+    tweets = (Tweet.objects.filter(user=user_).select_related('user').order_by('-created'))[:30]
     return render(request, 'twttr/base.html', {'tweets': tweets})
 
 
